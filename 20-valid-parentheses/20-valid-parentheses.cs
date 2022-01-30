89efbaac66f5b458e6f40c753b1fa705015a1e1d
Chaitanya <32837596+Chaitanya142@users.noreply.github.com>
2022-01-30 13:04:21 +0530

    Time: 89 ms (54.46%), Space: 36.4 MB (55.45%)

--- 20-valid-parentheses/20-valid-parentheses.cs
+++ 20-valid-parentheses/20-valid-parentheses.cs
@@ -2,63 +2,22 @@ public class Solution
 {
     public bool IsValid(string s)
     {
-        Console.WriteLine(s.Length);
-        Console.WriteLine(s[0]);
-        int length = s.Length;
-        if (length % 2 == 1)
-            return false;
-        //int count=0;
-        //while(count<s.Length/2){
-        //  if(isValidClosingChar(s[count],s[length-count-1])){
-        //    count++;
-        // }else{
-        //    return false;
-        // }
-        //}
-        Stack<char> myStack = new Stack<char>();
+        
+        Stack<char> stack = new Stack<char>();
 
         foreach (char c in s)
         {
-            char m = getOpeningChar(c);
-            if (m == ' ')
-                myStack.Push(c);
-            else
-            {
-                if (myStack.Count == 0)
-                {
-                    return false;
-                }
-                char n = myStack.Pop();
-                if (n != m)
-                    return false;
-            }
+            if (c == '(')
+			    stack.Push(')');
+		    else if (c == '{')
+			    stack.Push('}');
+		    else if (c == '[')
+			    stack.Push(']');
+		    else if (stack.Count == 0 || stack.Pop() != c)
+			return false;
         }
 
-        if (myStack.Count > 0)
-            return false;
-        return true;
-    }
-
-    /*public char getClosingChar(char s){
-        switch(s){
-            case '{': return '}';
-            case '(': return ')';
-            case '[':return ']';
-            default:return ' ';
-                break;
-        }
-    }*/
-
-    public char getOpeningChar(char s)
-    {
-        switch (s)
-        {
-            case '}': return '{';
-            case ')': return '(';
-            case ']': return '[';
-            default: return ' ';
-
-        }
+        return stack.Count == 0;
     }
 
 }
