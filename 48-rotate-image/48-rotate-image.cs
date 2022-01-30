af1d6923aabd0a8b721fa0aeeb45b802457d9a6d
Chaitanya <32837596+Chaitanya142@users.noreply.github.com>
2022-01-30 13:34:44 +0530

    Time: 128 ms (68.50%), Space: 42.1 MB (5.58%)

--- 48-rotate-image/48-rotate-image.cs
+++ 48-rotate-image/48-rotate-image.cs
@@ -1,34 +1,38 @@
-/*
- * clockwise rotate
- * first swap the symmetry (i.e. transpose the matrix), then reverse each row
- * 1 2 3     1 4 7     7 4 1
- * 4 5 6  => 2 5 8  => 8 5 2
- * 7 8 9     3 6 9     9 6 3
- * 
- * anti-clockwise rotate
- * first swap the symmetry (i.e. transpose the matrix), then reverse each col
-*/
+
 class Solution
 {
     public void Rotate(int[][] matrix)
     {
-        int s = 0, e = matrix.Length - 1;
-        while (s < e)
-        {
-            int[] temp = matrix[s];
-            matrix[s] = matrix[e];
-            matrix[e] = temp;
-            s++; e--;
-        }
+        
+        int l = 0;
+        int r = matrix.Length - 1;
 
-        for (int i = 0; i < matrix.Length; i++)
+        while (l < r)
         {
-            for (int j = i + 1; j < matrix[i].Length; j++)
+            for (int i = 0; i < r - l; i++)
             {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                int t = l;
+                int b = r;
+
+                // store top left
+                int temp = matrix[t][l + i];
+
+                // move bottom left to top right
+                matrix[t][l + i] = matrix[b - i][l];
+
+                // move bottom right to bottom left
+                matrix[b - i][l] = matrix[b][r - i];
+
+                // move top right to botom right
+                matrix[b][r - i] = matrix[t + i][r];
+
+                // move top left to top right
+                matrix[t + i][r] = temp;
             }
+
+            l++;
+            r--;
         }
+        
     }
 }
