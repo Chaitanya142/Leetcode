defae7762c189cc43ab16a3292074b54cb86ba1d
Chaitanya <32837596+Chaitanya142@users.noreply.github.com>
2022-02-02 23:38:39 +0530

    Time: 399 ms (8.75%), Space: 49.8 MB (40.67%)

---
+++ 49-group-anagrams/49-group-anagrams.cs
@@ -0,0 +1,43 @@
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+
+        IList<IList<string>> lst = new List<IList<string>>();
+
+        var distinctStrs = strs;//.Distinct();
+
+        if (distinctStrs.Count() == 0)
+        {
+            return new List<IList<string>>() { new List<string>() { "" } };
+        }
+
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+        foreach (string str in distinctStrs)
+        {
+            var chars = getChars(str);
+            if (!dict.ContainsKey(chars)) dict.Add(chars, new List<string>());
+            dict[chars].Add(str);
+        }
+
+        foreach (var v in dict.Values.ToList())
+        {
+            lst.Add(v);
+        }
+        return lst;
+    }
+
+    public string getChars(string s)
+    {
+
+        int[] arr = new int[26];
+        int aChar = (int)'a';
+
+        foreach (char c in s)
+        {
+            arr[(int)c - aChar]++;
+        }
+        return string.Join(",", arr);
+    }
+}
