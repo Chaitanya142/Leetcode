d54e2765d37e63c13a92c47124b25e67631b6898
Chaitanya <32837596+Chaitanya142@users.noreply.github.com>
2022-02-03 23:17:15 +0530

    Time: 373 ms (21.64%), Space: 48.1 MB (22.30%)

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,47 +1,19 @@
-//Divide and concure
+//DP
 public class Solution {
-
-      public int MaxSubArray(int[] nums)
+ public int MaxSubArray(int[] nums)
     {
-
-        return helper(nums, 0, nums.Length - 1);
-    }
-
-    public int helper(int[] nums, int i, int j)
-    {
-
-        if (i == j)
+        int[] storage = new int[nums.Length];
+        int max = nums[0];
+        storage[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
         {
-            return nums[i];
-        }
-
-        int mid = (i + j) / 2;
-        int sum = 0, leftMax = int.MinValue;
-
-        for (int l = mid; l >= i; l--)
-        {
-            sum += nums[l];
-            if (sum > leftMax)
+            storage[i] = Math.Max(storage[i - 1] + nums[i], nums[i]);
+            if (storage[i] > max)
             {
-                leftMax = sum;
+                max = storage[i];
             }
         }
-
-        int rightMax = int.MinValue;
-        sum = 0;    // reset sum to 0
-        for (int l = mid + 1; l <= j; l++)
-        {
-            sum += nums[l];
-            if (sum > rightMax)
-            {
-                rightMax = sum;
-            }
-        }
-
-        int maxLeftRight = Math.Max(helper(nums, i, mid),
-                                     helper(nums, mid + 1, j));
-        return Math.Max(maxLeftRight, leftMax + rightMax);
-
+        return max;
     }
 }
 
