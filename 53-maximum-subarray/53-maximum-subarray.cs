b6b6e8bf07b34042b1a7458b54aaa303439ad07d
Chaitanya <32837596+Chaitanya142@users.noreply.github.com>
2022-02-03 23:13:52 +0530

    Time: 385 ms (16.52%), Space: 49.1 MB (6.56%)

--- 53-maximum-subarray/53-maximum-subarray.cs
+++ 53-maximum-subarray/53-maximum-subarray.cs
@@ -1,17 +1,48 @@
-//Kanade
+//Divide and concure
 public class Solution {
-    public int MaxSubArray(int[] nums) {
-        
-        int max = int.MinValue, sum = 0;
 
-        foreach(var no in nums)
+      public int MaxSubArray(int[] nums)
+    {
+
+        return helper(nums, 0, nums.Length - 1);
+    }
+
+    public int helper(int[] nums, int i, int j)
+    {
+
+        if (i == j)
         {
-            sum += no;
-            max = Math.Max(sum, max);
-            if(sum<0)sum=0;
+            return nums[i];
         }
 
-        return max;
-    
+        int mid = (i + j) / 2;
+        int sum = 0, leftMax = int.MinValue;
+
+        for (int l = mid; l >= i; l--)
+        {
+            sum += nums[l];
+            if (sum > leftMax)
+            {
+                leftMax = sum;
+            }
+        }
+
+        int rightMax = int.MinValue;
+        sum = 0;    // reset sum to 0
+        for (int l = mid + 1; l <= j; l++)
+        {
+            sum += nums[l];
+            if (sum > rightMax)
+            {
+                rightMax = sum;
+            }
+        }
+
+        int maxLeftRight = Math.Max(helper(nums, i, mid),
+                                     helper(nums, mid + 1, j));
+        return Math.Max(maxLeftRight, leftMax + rightMax);
+
     }
 }
+
+
